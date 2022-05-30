879abfb6f6b241c3b17086f46fbe5d029118db10
Iago Gondim <iago-gondim@hotmail.com>
2022-05-30 18:32:30 -0300

    Project created

---
+++ src/test/java/com/gondim/demo/DemoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gondim.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
