2ddb80dcc6ab38942c2c96c98d96df488a648305
Iago Gondim <iago-gondim@hotmail.com>
2022-05-31 17:52:28 -0300

    Endpoint /clientes/{id} disponivel

--- src/main/java/com/gondim/demo/resources/ClienteResources.java
+++ src/main/java/com/gondim/demo/resources/ClienteResources.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import com.gondim.demo.domain.Categoria;
-import com.gondim.demo.services.CategoriaService;
+import com.gondim.demo.domain.Cliente;
+import com.gondim.demo.services.ClienteService;
 
 @RestControllerAdvice
-@RequestMapping(value = "/categorias")
-public class CategoriaResources {
+@RequestMapping(value = "/clientes")
+public class ClienteResources {
 
 	@Autowired
-	private CategoriaService service;
+	private ClienteService service;
 	
 	@RequestMapping(value="/{id}",method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Cliente obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/gondim/demo/services/ClienteService.java
@@ -0,0 +1,22 @@
+package com.gondim.demo.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.gondim.demo.domain.Cliente;
+import com.gondim.demo.repositories.ClienteRepository;
+import com.gondim.demo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+
+	}
+}
