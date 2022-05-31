aaa1cd0550c72d5094ddbe91c4b68b712a278aed
Iago Gondim <iago-gondim@hotmail.com>
2022-05-31 09:01:59 -0300

    Criado classe produto e a associação de muitos para muitos com a classe categoria

--- src/main/java/com/gondim/demo/DemoJavaApplication.java
+++ src/main/java/com/gondim/demo/DemoJavaApplication.java
@@ -8,14 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gondim.demo.domain.Categoria;
+import com.gondim.demo.domain.Produto;
 import com.gondim.demo.repositories.CategoriaRepository;
+import com.gondim.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class DemoJavaApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJavaApplication.class, args);
 	}
@@ -25,8 +29,20 @@ public class DemoJavaApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
