3217ca88619207d2231742001428c7caccd472db
Iago Gondim <iago-gondim@hotmail.com>
2022-05-31 11:29:42 -0300

    Estado e cidade

--- src/main/java/com/gondim/demo/DemoJavaApplication.java
+++ src/main/java/com/gondim/demo/DemoJavaApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.gondim.demo.domain.Categoria;
+import com.gondim.demo.domain.Cidade;
+import com.gondim.demo.domain.Estado;
 import com.gondim.demo.domain.Produto;
 import com.gondim.demo.repositories.CategoriaRepository;
+import com.gondim.demo.repositories.CidadeRepository;
+import com.gondim.demo.repositories.EstadoRepository;
 import com.gondim.demo.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class DemoJavaApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJavaApplication.class, args);
@@ -43,6 +51,20 @@ public class DemoJavaApplication implements CommandLineRunner {
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		Estado est1 = new Estado(null, "Ceará");
+		Estado est2 = new Estado(null, "Pernambuco");
+
+		Cidade c1 = new Cidade(null, "Juazeiro do Norte", est1);
+		Cidade c2 = new Cidade(null, "Salgueiro", est2);
+		Cidade c3 = new Cidade(null, "Recife", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 
 }
