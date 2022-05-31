c77d039a417abfa0a65789f4166a35d3267b0b8c
Iago Gondim <iago-gondim@hotmail.com>
2022-05-30 21:20:21 -0300

    Operação de instanciação criada

--- src/main/java/com/gondim/demo/DemoJavaApplication.java
+++ src/main/java/com/gondim/demo/DemoJavaApplication.java
@@ -1,13 +1,32 @@
 package com.gondim.demo;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DemoJavaApplication {
+import com.gondim.demo.domain.Categoria;
+import com.gondim.demo.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class DemoJavaApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoJavaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
