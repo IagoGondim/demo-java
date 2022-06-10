4d052c640cba9f111761325aaaf264698d313597
Iago Gondim <iago-gondim@hotmail.com>
2022-06-10 11:34:48 -0300

    Inserindo uma nova Categoria com POST

--- src/main/java/com/gondim/demo/resources/CategoriaResources.java
+++ src/main/java/com/gondim/demo/resources/CategoriaResources.java
@@ -1,11 +1,15 @@
 package com.gondim.demo.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gondim.demo.domain.Categoria;
 import com.gondim.demo.services.CategoriaService;
@@ -16,10 +20,18 @@ public class CategoriaResources {
 
 	@Autowired
 	private CategoriaService service;
-	
-	@RequestMapping(value="/{id}",method = RequestMethod.GET)
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+	return ResponseEntity.created(uri).build();
+	}
+
 }
