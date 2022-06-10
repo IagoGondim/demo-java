7f377e8dafcf54ad77c1c53fcd2b17afad57874a
Iago Gondim <iago-gondim@hotmail.com>
2022-06-10 16:06:21 -0300

    Lista de todas as categorias com DTO

--- src/main/java/com/gondim/demo/resources/CategoriaResources.java
+++ src/main/java/com/gondim/demo/resources/CategoriaResources.java
@@ -1,6 +1,8 @@
 package com.gondim.demo.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.gondim.demo.domain.Categoria;
+import com.gondim.demo.dto.CategoriaDTO;
 import com.gondim.demo.services.CategoriaService;
+
+
 
 @RestControllerAdvice
 @RequestMapping(value = "/categorias")
@@ -46,5 +51,11 @@ public class CategoriaResources {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
-
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
+	}
 }
