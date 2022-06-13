28051f664fe23cf8bfa38cc7f04064cfddd7f943
Iago Gondim <iago-gondim@hotmail.com>
2022-06-13 09:54:32 -0300

    Paginacao com paramentos opcionais na requisicao

--- src/main/java/com/gondim/demo/services/CategoriaService.java
+++ src/main/java/com/gondim/demo/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.gondim.demo.domain.Categoria;
@@ -43,9 +46,15 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
 	}
-	
-	public List<Categoria> findAll(){
+
+	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){ 
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), 
+			orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
