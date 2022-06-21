e3192a8c99a6c2bd7072c11e56c2e5acfd485b31
Iago Gondim <iago-gondim@hotmail.com>
2022-06-21 17:34:46 -0300

    Validacao customizada email nao repeitdo na insercao de cliente

--- src/main/java/com/gondim/demo/repositories/ClienteRepository.java
+++ src/main/java/com/gondim/demo/repositories/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.gondim.demo.repositories;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import com.gondim.demo.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 	
 }
--- src/main/java/com/gondim/demo/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/gondim/demo/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gondim.demo.domain.Cliente;
 import com.gondim.demo.domain.enums.TipoCliente;
 import com.gondim.demo.dto.ClienteNewDTO;
+import com.gondim.demo.repositories.ClienteRepository;
 import com.gondim.demo.resources.exception.FieldMessage;
 import com.gondim.demo.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ClienteNewDTO) {
 	}
@@ -28,6 +35,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 
 		for (FieldMessage e : list) {
