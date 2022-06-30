645e602e2b158392a27e7fb849c69420167c5e71
Iago Gondim <iago-gondim@hotmail.com>
2022-06-30 12:24:09 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/gondim/demo/repositories/ProdutoRepository.java
+++ src/main/java/com/gondim/demo/repositories/ProdutoRepository.java
@@ -1,12 +1,21 @@
 package com.gondim.demo.repositories;
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.gondim.demo.domain.Categoria;
 import com.gondim.demo.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
-	
+	@Transactional(readOnly = true)
+
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,
+			Pageable pageRequest);
 }
